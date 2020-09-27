09f0ea8c60984692de9f91a37c53faf78cd17d08
git <627861816@qq.com>
2020-09-28 00:31:24 +0800

    6方法完

--- Go程序设计语言/6方法/intset.go
+++ Go程序设计语言/6方法/intset.go
@@ -1,7 +1,25 @@
 package main
 
-func main() {
+import (
+	"bytes"
+	"fmt"
+)
 
+func main() {
+	fmt.Sprint()
+	var x, y IntSet
+	x.Add(1)
+	x.Add(144)
+	x.Add(9)
+	fmt.Println(x.String())
+
+	y.Add(9)
+	y.Add(42)
+	fmt.Println(y.String())
+
+	x.UnionWith(&y)
+	fmt.Println(x.String())
+	fmt.Println(x.Has(9), x.Has(123))
 }
 
 type IntSet struct {
@@ -11,4 +29,40 @@ type IntSet struct {
 func (s *IntSet) Has(x int) bool {
 	word, bit := x/64, uint(x%64)
 	return word < len(s.words) && s.words[word]&(1<<bit)!=0
+}
+
+func (s *IntSet)  Add(x int)  {
+	word, bit := x/64, uint(x%64)
+	for word >= len(s.words) {
+		s.words = append(s.words, 0)
+	}
+	s.words[word] |= 1 <<bit
+}
+
+func (s *IntSet) UnionWith(t *IntSet) {
+	for i, tword := range t.words{
+		if i<len(s.words){
+			s.words[i] |= tword
+		}else {
+			s.words = append(s.words, tword)
+		}
+	}
+}
+
+func (s *IntSet) String() string {
+	var buf bytes.Buffer
+	buf.WriteByte('{')
+	for i, word := range s.words{
+		if word == 0 {
+			continue
+		}
+		for j := 0;j<64; j++{
+			if word&(1<<uint(j)) != 0 {
+				buf.WriteByte(' ')
+			}
+			fmt.Fprintf(&buf, "%d", 64*i + j)
+		}
+	}
+	buf.WriteByte('}')
+	return buf.String()
 }
