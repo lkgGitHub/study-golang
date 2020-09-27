356f5296672c43b1bda3505a3f79db6fa9143fd9
李凯刚 <627861816@qq.com>
2020-09-27 21:58:25 +0800

    no message

---
+++ Go程序设计语言/6方法/intset.go
@@ -0,0 +1,14 @@
+package main
+
+func main() {
+
+}
+
+type IntSet struct {
+	words []uint64
+}
+
+func (s *IntSet) Has(x int) bool {
+	word, bit := x/64, uint(x%64)
+	return word < len(s.words) && s.words[word]&(1<<bit)!=0
+}
