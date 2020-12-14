260c756b794429a9851fa569a6772a44be7834a1
lkg-macMini <627861816@qq.com>
2020-12-15 01:37:06 +0800

    no message

---
+++ dgInterview/0.语法题/main_test.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func Test1(t *testing.T) {
+	var x int
+	inc := func() int {
+		x++
+		return x
+	}
+	// result: 1 2
+	fmt.Println(func() (a, b int) {
+		return inc(), inc()
+	}())
+}
