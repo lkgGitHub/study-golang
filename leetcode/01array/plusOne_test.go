86c237b568dd5d09ded0ee2456f019d99e15b375
lkg-macMini <627861816@qq.com>
2021-03-12 00:06:57 +0800

    加1 ，只用一个slice

--- leetcode/01array/plusOne_test.go
+++ leetcode/01array/plusOne_test.go
@@ -8,6 +8,8 @@ import (
 /*
 加一
 
+tag: 数组
+
 给定一个由 整数 组成的 非空 数组所表示的非负整数，在该数的基础上加一。
 最高位数字存放在数组的首位， 数组中每个元素只存储单个数字。
 你可以假设除了整数 0 之外，这个整数不会以零开头。
@@ -33,8 +35,9 @@ import (
 0 <= digits[i] <= 9
 
 */
+
 // 数组
-func plusOne(digits []int) []int {
+func plusOne2(digits []int) []int {
 
 	for i := len(digits) - 1; i >= 0; i-- {
 		if digits[i]+1 < 10 {
@@ -51,6 +54,23 @@ func plusOne(digits []int) []int {
 	return nums
 }
 
+// 数组
+func plusOne(digits []int) []int {
+
+	for i := len(digits) - 1; i >= 0; i-- {
+		if digits[i]+1 < 10 {
+			digits[i] = digits[i] + 1
+			return digits
+		} else {
+			digits[i] = 0
+		}
+	}
+	// 此时 digits 所有位都是0
+	digits = append(digits, 0)
+	digits[0] = 1
+	return digits
+}
+
 func TestPlusOne(t *testing.T) {
 	tests := []struct {
 		digits   []int
@@ -63,7 +83,7 @@ func TestPlusOne(t *testing.T) {
 		{[]int{9, 9, 9}, []int{1, 0, 0, 0}},
 	}
 	for _, tt := range tests {
-		actual := plusOne(tt.digits)
+		actual := plusOne2(tt.digits)
 		isPass := true
 		for i := 0; i < len(actual); i++ {
 			if actual[i] != tt.expected[i] {
