60308197f5de9c4bca725ddeea2b2ce3c0f1b540
lkg <627861816@qq.com>
2021-03-21 21:50:00 +0800

    no message

--- leetcode/02string/strStr_test.go
+++ leetcode/02string/strStr_test.go
@@ -9,6 +9,7 @@ import (
 /*
 实现 strStr()
 // todo: Rabin Karp - 常数复杂度,暂时放弃
+// todo: KMP 算法详解
 实现strStr()函数。
 给定一个haystack 字符串和一个 needle 字符串，在 haystack 字符串中找出 needle 字符串出现的第一个位置 (从0开始)。如果不存在，则返回  -1。
 
