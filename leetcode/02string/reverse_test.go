7a905d2389b1659902e70d212db295368200b3c8
lkg <627861816@qq.com>
2021-03-22 13:50:39 +0800

    整数反转

---
+++ leetcode/02string/reverse_test.go
@@ -0,0 +1,84 @@
+package string
+
+import (
+	"fmt"
+	"math"
+	"strconv"
+	"strings"
+	"testing"
+)
+
+/*
+整数反转
+
+给你一个 32 位的有符号整数 x ，返回将 x 中的数字部分反转后的结果。
+如果反转后整数超过 32 位的有符号整数的范围[−2^31, 2^31− 1] ，就返回 0。
+假设环境不允许存储 64 位整数（有符号或无符号）。
+
+
+示例 1：
+输入：x = 123
+输出：321
+
+示例 2：
+输入：x = -123
+输出：-321
+
+示例 3：
+输入：x = 120
+输出：21
+
+示例 4：
+输入：x = 0
+输出：0
+
+提示：
+-231 <= x <= 231 - 1
+
+相关标签:	数学
+*/
+
+// 转换成字符串，利用字符串反转。最后再转成int32，超过int32的转换过程中会有error，直接返回0
+func reverse(x int) int {
+	xStr := strconv.Itoa(x)
+	yStr := strings.Builder{}
+	if xStr[0] == '-' {
+		yStr.WriteRune('-')
+		xStr = xStr[1:]
+	}
+	for i := len(xStr) - 1; i >= 0; i-- {
+		yStr.WriteByte(xStr[i])
+	}
+	y, err := strconv.ParseInt(yStr.String(), 10, 32)
+	if err != nil {
+		return 0
+	}
+	return int(y)
+}
+func TestReverse(t *testing.T) {
+	tests := []struct {
+		x        int
+		expected int
+	}{
+		{123, 321},
+		{-123, -321},
+		{120, 21},
+		{0, 0},
+	}
+	for _, tt := range tests {
+		actual := reverse(tt.x)
+		if tt.expected != actual {
+			t.Error(fmt.Sprintf("reverse failed. expect: %v , actual: %v", tt.expected, actual))
+		}
+	}
+}
+
+func TestReverseDemo(t *testing.T) {
+	maxInt64 := math.MaxInt32
+	i, err := strconv.ParseInt(strconv.Itoa(maxInt64+1), 10, 32)
+	if err != nil {
+		t.Error(err.Error())
+		return
+	}
+	println("===>", i)
+}
--- leetcode/03linkedList/deleteNode_test.go
+++ leetcode/03linkedList/deleteNode_test.go
@@ -1,1 +1,68 @@
-package _3linkedList
+package linkedList
+
+import (
+	"fmt"
+	"testing"
+)
+
+/*
+删除链表中的节点
+
+这道题感觉很扯，因为head和ListNode在官方代码中。线下手撸没法测试。
+线下自己构造出head和ListNode 撸完又不对。哎~理解思想就好
+*/
+// 提交的代码
+func deleteNode(node *ListNode) {
+	node.Val = node.Next.Val
+	node.Next = node.Next.Next
+}
+
+//Definition for singly-linked list.
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+var head = &ListNode{4, &ListNode{5, &ListNode{1, &ListNode{9, nil}}}}
+
+// 实际逻辑和测试
+func myDeleteNode(node *ListNode) {
+	h := &ListNode{head.Val, head.Next}
+	for ; h.Next != nil; h = h.Next {
+		if h.Val == node.Val {
+			next := h.Next
+			h.Val = next.Val
+			h.Next = next.Next
+			break
+		}
+	}
+}
+
+func TestDeleteNode(t *testing.T) {
+	tests := []struct {
+		node     *ListNode
+		expected *ListNode
+	}{
+		{&ListNode{5, nil},
+			&ListNode{4, &ListNode{1, &ListNode{9, nil}}}},
+
+		{
+			&ListNode{1, nil},
+			&ListNode{4, &ListNode{5, &ListNode{9, nil}}},
+		},
+	}
+	for _, tt := range tests {
+		head = &ListNode{4, &ListNode{5, &ListNode{1, &ListNode{9, nil}}}}
+		deleteNode(tt.node)
+		isPass := true
+		for ; head.Next != nil; head, tt.expected = head.Next, tt.expected.Next {
+			if head.Val != tt.expected.Val {
+				isPass = false
+				break
+			}
+		}
+		if !isPass {
+			t.Error(fmt.Sprintf("TestRotate failed. node: %d, actual: %v", tt.node.Val, head))
+		}
+	}
+}
