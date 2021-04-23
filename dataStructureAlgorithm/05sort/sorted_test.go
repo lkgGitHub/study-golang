7ab060b68d0a95dd9ef80738cca2b3d8da7e8d82
lkg <627861816@qq.com>
2021-04-24 00:42:13 +0800

    插入排序 and 选择排序

--- dataStructureAlgorithm/05sort/sorted_test.go
+++ dataStructureAlgorithm/05sort/sorted_test.go
@@ -27,21 +27,44 @@ func bubbleSort(nums []int) []int {
 
 // 插入排序（Insertion Sort）
 func insertionSort(nums []int) []int {
-	if len(nums) < 2 {
+	if len(nums) <= 1 {
 		return nums
 	}
 
 	for i := 1; i < len(nums); i++ {
-		flag := false
-		for j := 0; j < i; j++ {
-			if nums[i] > nums[j] {
-				nums[i], nums[j] = nums[j], nums[i]
+		value := nums[i]
+		j := i - 1
+		for ; j >= 0; j-- {
+			if nums[j] > value {
+				nums[j+1] = nums[j]
+			} else {
+				break
 			}
 		}
-		if flag {
+		// 注意：相对于nums[j+1] = nums[j]，已经又走了一论，现在的j比最后一个这行表达式里的j小1，所以再加1才是现在的j
+		nums[j+1] = value
+	}
 
+	return nums
+}
+
+// 选择排序(写着就感觉比冒泡和插入排序复杂）
+func selectionSort(nums []int) []int {
+	if len(nums) <= 1 {
+		return nums
+	}
+
+	for i := 0; i < len(nums)-1; i++ {
+		min := nums[i]
+		a := i
+		b := i
+		for j := i + 1; j < len(nums); j++ {
+			if nums[j] < min {
+				min = nums[j]
+				b = j
+			}
 		}
-
+		nums[a], nums[b] = nums[b], nums[a]
 	}
 	return nums
 }
@@ -52,14 +75,16 @@ func TestName(t *testing.T) {
 		expected []int
 	}{
 		{[]int{5, 4, 3, 2, 1}, []int{1, 2, 3, 4, 5}},
+		{[]int{4, 5, 6, 1, 3, 2}, []int{1, 2, 3, 4, 5, 6}},
 		{[]int{1}, []int{1}},
 		{[]int{}, []int{}},
 	}
 	for _, tt := range tests {
-		actual := bubbleSort(tt.nums)
+		actual := selectionSort(tt.nums)
 		isPass := true
 		if len(actual) != len(tt.expected) {
-			panic("")
+			t.Errorf("sorted failed. expect: %v, actual: %v", tt.expected, actual)
+			continue
 		}
 		for i := 0; i < len(actual); i++ {
 			if tt.expected[i] != actual[i] {
