71d074110f34727c0e2d2678c78995c8e4a40c27
lkg-home <>
2022-02-16 01:11:39 +0800

    三数之和

---
+++ leetcode/threeSum_test.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_threeSum(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want [][]int
+	}{
+		// TODO: Add test cases.
+		{"示例 1", args{[]int{-1, 0, 1, 2, -1, -4}}, [][]int{{-1, -1, 2}, {-1, 0, 1}}},
+		{"示例 2", args{[]int{}}, [][]int{}},
+		{"示例 3", args{[]int{0}}, [][]int{}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := threeSum(tt.args.nums); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("threeSum() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func threeSum(nums []int) [][]int {
+
+	return nil
+}
