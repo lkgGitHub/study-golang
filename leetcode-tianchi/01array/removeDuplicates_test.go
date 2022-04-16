b9ad9bd505c5a4ef5c960313049cc1ae231aed36
lkg-home <>
2022-04-16 09:46:24 +0800

    删除有序数组中的重复项

---
+++ leetcode-tianchi/01array/removeDuplicates_test.go
@@ -0,0 +1,124 @@
+package array
+
+import (
+	"fmt"
+	"testing"
+)
+
+/*
+26. 删除有序数组中的重复项
+
+给你一个 升序排列 的数组 nums ，请你 原地 删除重复出现的元素，使每个元素 只出现一次 ，返回删除后数组的新长度。元素的 相对顺序 应该保持 一致 。
+由于在某些语言中不能改变数组的长度，所以必须将结果放在数组nums的第一部分。更规范地说，如果在删除重复项之后有 k 个元素，那么 nums 的前 k 个元素应该保存最终结果。
+将最终结果插入 nums 的前 k 个位置后返回 k 。
+不要使用额外的空间，你必须在 原地 修改输入数组 并在使用 O(1) 额外空间的条件下完成。
+
+判题标准:
+系统会用下面的代码来测试你的题解:
+```
+	int[] nums = [...]; // 输入数组
+	int[] expectedNums = [...]; // 长度正确的期望答案
+
+	int k = removeDuplicates(nums); // 调用
+
+	assert k == expectedNums.length;
+	for (int i = 0; i < k; i++) {
+		assert nums[i] == expectedNums[i];
+	}
+```
+如果所有断言都通过，那么您的题解将被 通过。
+
+示例 1：
+输入：nums = [1,1,2]
+输出：2, nums = [1,2,_]
+解释：函数应该返回新的长度 2 ，并且原数组 nums 的前两个元素被修改为 1, 2 。不需要考虑数组中超出新长度后面的元素。
+
+示例 2：
+输入：nums = [0,0,1,1,1,2,2,3,3,4]
+输出：5, nums = [0,1,2,3,4]
+解释：函数应该返回新的长度 5 ， 并且原数组 nums 的前五个元素被修改为 0, 1, 2, 3, 4 。不需要考虑数组中超出新长度后面的元素。
+
+
+提示：
+0 <= nums.length <= 3 * 104
+-104 <= nums[i] <= 104
+nums 已按 升序 排列
+
+*/
+func TestRemoveDuplicates(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+		{"示例 1", args{[]int{1, 1, 2}}, 2},
+		{"示例 2", args{[]int{0, 0, 1, 1, 1, 2, 2, 3, 3, 4}}, 5},
+		{"示例 3", args{[]int{1, 1}}, 1},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := removeDuplicates(tt.args.nums); got != tt.want {
+				t.Errorf("removeDuplicates() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+// 方法一：双指针
+func removeDuplicates(nums []int) int {
+	if len(nums) == 0 {
+		return 0
+	}
+	slow := 1
+	for fast := 1; fast < len(nums); fast++ {
+		if nums[fast] != nums[fast-1] {
+			nums[slow] = nums[fast]
+			slow++
+		}
+	}
+
+	return slow
+}
+
+func removeDuplicates1(nums []int) int {
+	if len(nums) == 0 {
+		return 0
+	}
+	n := nums[0]
+	for i := 1; i < len(nums); {
+		if n == nums[i] {
+			// 该操作因为涉及 i 后所有元素引动，比较耗时
+			nums = append(nums[:i], nums[i+1:]...)
+		} else {
+			n = nums[i]
+			i++
+		}
+	}
+
+	fmt.Println(nums)
+	return len(nums)
+}
+
+// 新的数组
+func removeDuplicates2(nums []int) int {
+	if len(nums) == 0 {
+		return 0
+	}
+	var nums2 []int
+	nums2 = append(nums2, nums[0])
+	n := nums[0]
+	for i := 1; i < len(nums); i++ {
+		if n != nums[i] {
+			nums2 = append(nums2, nums[i])
+			n = nums[i]
+		}
+	}
+
+	copy(nums, nums2)
+	fmt.Println(nums2)
+	return len(nums2)
+}
