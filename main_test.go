0d47e44017465734aa7bb8695f488a5719866ca6
lkg-home <>
2022-09-16 09:33:48 +0800

    1

--- main_test.go
+++ main_test.go
@@ -20,3 +20,19 @@ func Bytes2Bits(data []byte) []uint8 {
 	fmt.Println(len(dst))
 	return dst
 }
+
+func TestStringer(t *testing.T) {
+	//var s fmt.Stringer
+	//s = "s"
+	//fmt.Println(s)
+	// 编译错误
+
+	const X = 7.0
+	var x interface{} = X
+	if y, ok := x.(int); ok {
+		fmt.Println(y)
+	} else {
+		fmt.Println(int(y))
+	}
+	// 最后输出的是 0，因为 y 类型断言失败，则 y 为 int 的零值
+}
