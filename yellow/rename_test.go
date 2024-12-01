ef72e5e117721739237550367872122188909cfa
lkg <627861816@qq.com>
2024-12-02 00:21:43 +0800

    TestCheckName

--- yellow/rename_test.go
+++ yellow/rename_test.go
@@ -1,39 +1,18 @@
 package yellow
 
 import (
+	"bufio"
 	"fmt"
 	"log"
 	"os"
 	"path"
+	"path/filepath"
 	"strings"
 	"testing"
 )
 
 const reNameDir = ""
 
-func TestPrefix(t *testing.T) {
-	dirname := reNameDir
-	fileInfos, err := os.ReadDir(reNameDir)
-	if err != nil {
-		log.Fatal(err)
-	}
-	for _, fi := range fileInfos {
-		if !strings.HasSuffix(strings.ToLower(fi.Name()), ".mp4") {
-			continue
-		}
-		if strings.Contains(fi.Name(), prefix) && !strings.HasPrefix(fi.Name(), prefix) {
-			newName := strings.Replace(fi.Name(), prefix, "", -1)
-			newName, _ = deleteInvalid(newName)
-			err = os.Rename(path.Join(dirname, fi.Name()), path.Join(dirname, prefix+"-"+newName))
-			if err != nil {
-				fmt.Println(err.Error())
-				continue
-			} else {
-				fmt.Println(fi.Name(), "->", prefix+"-"+newName)
-			}
-		}
-	}
-}
 
 func TestRename(t *testing.T) {
 	dirname := reNameDir
@@ -65,6 +44,94 @@ func TestRename(t *testing.T) {
 			}
 		}
 	}
+}
+
+func TestCheckName(t *testing.T) {
+	files, err := getAllFile(reNameDir)
+	if err != nil {
+		t.Error(err.Error())
+		return
+	}
+	fileMap := make(map[string]string, len(files))
+	for _, f := range files {
+		n := filepath.Base(f)
+		if v, ok := fileMap[n]; !ok {
+			fileMap[n] = f
+		} else {
+			fmt.Printf("name: %s \n %s\n %s \n", n, v, f)
+		}
+	}
+
+	body, err := os.ReadFile("yellow.txt")
+	if err != nil {
+		panic(err)
+	}
+	bodyMap := make(map[string]struct{}, 3000)
+	for _, line := range strings.Split(string(body), "\n") {
+		bodyMap[line] = struct{}{}
+	}
+
+	var content strings.Builder
+	for k := range fileMap {
+		if _, ok := bodyMap[k]; !ok {
+			content.WriteString(k)
+			content.WriteString("\n")
+		}
+	}
+
+	file, err := os.OpenFile("yellow.txt", os.O_WRONLY|os.O_APPEND, 0666)
+	if err != nil {
+		panic(err)
+	}
+	defer func() { _ = file.Close() }()
+	write := bufio.NewWriter(file)
+	if _, err = write.WriteString(content.String()); err != nil {
+		panic(err)
+	}
+	//Flush将缓存的文件真正写入到文件中
+	if err = write.Flush(); err != nil {
+		panic(err)
+	}
+
+}
+
+// 递归获取指定目录下的所有文件名
+func getAllFile(pathname string) ([]string, error) {
+	result := make([]string, 0, 1000)
+
+	fis, err := os.ReadDir(pathname)
+	if err != nil {
+		fmt.Printf("读取文件目录失败，pathname=%v, err=%v \n", pathname, err)
+		return result, err
+	}
+
+	// 所有文件/文件夹
+	for _, fi := range fis {
+		if strings.HasPrefix(fi.Name(), ".") {
+			continue
+		}
+		if strings.Contains(fi.Name(), "22,0622f7b76eba00") {
+			fmt.Println("aaaa")
+		}
+		if strings.HasSuffix(fi.Name(), ".jpg") || strings.HasSuffix(fi.Name(), ".jpeg") {
+			continue
+		}
+
+		fullName := pathname + "/" + fi.Name()
+		// 是文件夹则递归进入获取;是文件，则压入数组
+		if fi.IsDir() {
+			temp, err := GetAllFile(fullName)
+			if err != nil {
+				fmt.Printf("读取文件目录失败,fullname=%v, err=%v", fullName, err)
+				return result, err
+			}
+			result = append(result, temp...)
+		} else {
+			result = append(result, fullName)
+		}
+	}
+
+	return result, nil
 }
 
 func deleteRepetitionCharacter(name string) (string, bool) {
@@ -115,4 +182,28 @@ func deleteInvalid(name string) (string, bool) {
 	}
 
 	return name, rename
+}
+
+func TestPrefix(t *testing.T) {
+	dirname := reNameDir
+	fileInfos, err := os.ReadDir(reNameDir)
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, fi := range fileInfos {
+		if !strings.HasSuffix(strings.ToLower(fi.Name()), ".mp4") {
+			continue
+		}
+		if strings.Contains(fi.Name(), prefix) && !strings.HasPrefix(fi.Name(), prefix) {
+			newName := strings.Replace(fi.Name(), prefix, "", -1)
+			newName, _ = deleteInvalid(newName)
+			err = os.Rename(path.Join(dirname, fi.Name()), path.Join(dirname, prefix+"-"+newName))
+			if err != nil {
+				fmt.Println(err.Error())
+				continue
+			} else {
+				fmt.Println(fi.Name(), "->", prefix+"-"+newName)
+			}
+		}
+	}
 }
